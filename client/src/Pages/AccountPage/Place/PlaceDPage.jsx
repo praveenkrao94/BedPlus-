f6ad9876de63a3cdd59ba772a88872e271b2dcef
praveenkr0812@gmail.com <praveen.mindnerves@gmail.com>
2023-06-26 20:33:07 +0530

    c

--- client/src/Pages/AccountPage/Place/PlaceDPage.jsx
+++ client/src/Pages/AccountPage/Place/PlaceDPage.jsx
@@ -27,12 +27,12 @@ return(
     <div className ='absolute inset-0  bg-black text-white min-h-screen'>
         <div className='p-8 grid gap-4 bg-black'>
             <div>
-                <h2 className='text-3xl' >Photos of {place.title}</h2>
+                <h2 className='text-3xl mr-48' >Photos of {place.title}</h2>
                 <button onClick={()=> setShowAllPhotos(false)} className='fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl bg-black text-white shadow shadow-black' ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                 <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clipRule="evenodd" />
                 </svg>
 
-                    close Photos</button>
+                    Close Photos</button>
             </div>
         {place?.photos?.length>0 && place.photos.map(photo=>(
         <div>
