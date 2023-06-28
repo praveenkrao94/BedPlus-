67d24ada81352b2320dfa06b15e16d6e895be1da
praveenkr0812@gmail.com <praveen.mindnerves@gmail.com>
2023-06-28 20:01:40 +0530

    c

--- client/src/Pages/AccountPage/Place/PlaceDPage.jsx
+++ client/src/Pages/AccountPage/Place/PlaceDPage.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import BookingWidget from './BookingWidget';
+import PlaceGalery from './PlaceGalery';
+import AddressLink from './AddressLink';
 
 function PlaceDPage() {
 
@@ -48,39 +50,8 @@ return(
   return (
     <div className='mt-4 bg-gray-100 -mx-8 px-8 pt-8' >
       <h1 className='text-3xl' >{place.title}</h1>
-      <a target='blank' href={'https://maps.google.com/?q='+place.address} className='flex gap-1 my-2 block font-semibold underline my-3' ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-            </svg>
-
-        
-        {place.address}</a>
-      <div className="relative">
-
-      <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
-        <div>
-            {place.photos?.[0] && (
-                <div>
-                    <img onClick={()=>setShowAllPhotos(true)} className='aspect-square object-cover cursor-pointer' src={'http://localhost:4000/uploads/'+place.photos[0]} alt="" />
-                </div>
-            )}
-        </div>
-        <div className='grid'>
-        {place.photos?.[1] && (
-                <img onClick={()=>setShowAllPhotos(true)} className='aspect-square object-cover  cursor-pointer' src={'http://localhost:4000/uploads/'+place.photos[1]} alt="" />
-            )}
-            <div className='overflow-hidden' >
-            {place.photos?.[2] && (
-                <img onClick={()=>setShowAllPhotos(true)} className='aspect-square object-cover relative top-2  cursor-pointer' src={'http://localhost:4000/uploads/'+place.photos[2]} alt="" />
-            )}
-            </div>
-        </div>
-      </div>
-      <button onClick={()=> setShowAllPhotos(true)} className='flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-            <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
-            </svg>
-More Photos</button>
-      </div>
+     <AddressLink>{place.address}</AddressLink>
+      <PlaceGalery place= {place}/>
      
       <div className='mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]'>
         
--- client/src/Pages/Booking/BookingSingle.jsx
+++ client/src/Pages/Booking/BookingSingle.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import AddressLink from '../AccountPage/Place/AddressLink';
+import PlaceGalery from '../AccountPage/Place/PlaceGalery';
+import BookingDates from './BookingDates';
 
 function BookingSingle() {
+  const { id } = useParams();
 
-    const {id} = useParams()
+  const [booking, setBooking] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      axios.get('/bookings').then(res => {
+        const foundBooking = res.data.find(({ _id }) => _id === id);
+        if (foundBooking) {
+          setBooking(foundBooking);
+        }
+      });
+    }
+  }, [id]);
+
+  if (!booking) {
+    return '';
+  }
+
   return (
-    <div>
-      BookingSingle:{id} 
-
+    <div className='my-8'>
+      <h1 className='text-3xl'>{booking.place.title}</h1>
+      <AddressLink className="my-2 block" >{booking.place.address}</AddressLink>
+      <div className="bg-gray-200 p-4 mb-4 rounded-2xl">
+        <h2 className='text-xl'>Your Booking Information:
+        <BookingDates booking={booking} />
+        </h2>
+      </div>
+      <PlaceGalery place={booking.place}/>
     </div>
-  )
+  );
 }
 
-export default BookingSingle
+export default BookingSingle;
