a3184c30312a465dab20239f1b652f53301821c1
praveenkr0812@gmail.com <praveen.mindnerves@gmail.com>
2023-06-28 21:04:20 +0530

    f

--- client/src/Pages/Booking/BookingSingle.jsx
+++ client/src/Pages/Booking/BookingSingle.jsx
@@ -5,15 +5,14 @@ import AddressLink from '../AccountPage/Place/AddressLink';
 import PlaceGalery from '../AccountPage/Place/PlaceGalery';
 import BookingDates from './BookingDates';
 
-function BookingSingle() {
-  const { id } = useParams();
 
-  const [booking, setBooking] = useState(null);
-
+export default function BookingSingle() {
+  const {id} = useParams();
+  const [booking,setBooking] = useState(null);
   useEffect(() => {
     if (id) {
-      axios.get('/bookings').then(res => {
-        const foundBooking = res.data.find(({ _id }) => _id === id);
+      axios.get('/bookings').then(response => {
+        const foundBooking = response.data.find(({_id}) => _id === id);
         if (foundBooking) {
           setBooking(foundBooking);
         }
@@ -26,17 +25,20 @@ function BookingSingle() {
   }
 
   return (
-    <div className='my-8'>
-      <h1 className='text-3xl'>{booking.place.title}</h1>
-      <AddressLink className="my-2 block" >{booking.place.address}</AddressLink>
-      <div className="bg-gray-200 p-4 mb-4 rounded-2xl">
-        <h2 className='text-xl'>Your Booking Information:
-        <BookingDates booking={booking} />
-        </h2>
+    <div className="my-8">
+      <h1 className="text-3xl">{booking.place.title}</h1>
+      <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
+      <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
+        <div>
+          <h2 className="text-2xl mb-4">Your booking information:</h2>
+          <BookingDates booking={booking} />
+        </div>
+        <div className="bg-primary p-6 text-white rounded-2xl">
+          <div>Total price</div>
+          <div className="text-3xl">${booking.price}</div>
+        </div>
       </div>
-      <PlaceGalery place={booking.place}/>
+      <PlaceGalery place={booking.place} />
     </div>
   );
 }
-
-export default BookingSingle;
